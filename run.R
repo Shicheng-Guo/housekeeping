938b7d5b32f53d8692c3dc860a03fa07ca672b58
Shicheng Guo <Shicheng.Guo@hotmail.com>
2020-01-22 20:12:47 -0600

    Update run.R

--- run.R
+++ run.R
@@ -1,5 +1,3 @@
-setwd("/home/guosa/hpc/project/esopheagul")
-setwd("/home/guosa/hpc/project/crc")
 setwd("/mnt/bigdata/Genetic/Projects/shg047/temp/housekeeping")
 data<-read.csv("https://raw.githubusercontent.com/Shicheng-Guo/esophageal/master/extdata/ESCC-UKBB-RSKAT2020.csv",as.is=T)
 data<-read.csv("https://raw.githubusercontent.com/Shicheng-Guo/colon/master/extdata/UKBB50K/CRC-UKBB-RSKAT2020.csv",as.is=T)
